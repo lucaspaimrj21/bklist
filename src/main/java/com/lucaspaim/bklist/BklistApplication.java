2aa610487b96eb06113da82823f92c24813dc470
Lucas Paim <lucaspaimrj21@gmail.com>
2025-02-11 20:49:11 -0300

    project created

---
+++ src/main/java/com/lucaspaim/bklist/BklistApplication.java
@@ -0,0 +1,13 @@
+package com.lucaspaim.bklist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BklistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BklistApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lucaspaim/bklist/BklistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucaspaim.bklist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BklistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
