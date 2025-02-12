f19fe6b9d8d1f752afc0bdb120042e0cb537096d
Lucas Paim <lucaspaimrj21@gmail.com>
2025-02-11 21:31:52 -0300

    properties, Book, ORM

---
+++ src/main/java/com/lucaspaim/bklist/entities/Book.java
@@ -0,0 +1,131 @@
+package com.lucaspaim.bklist.entities;
+
+import java.util.Objects;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table (name = "tb_book")
+public class Book {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	private String title;
+	private String publisher;
+	private Integer numberOfPages;
+	private Integer yearOfPublication;
+	
+	@Column(columnDefinition = "TEXT")
+	private String synopsis;
+	
+	private String isbn10;
+	private String isbn13;
+	
+	public Book () {
+		
+	}
+
+	public Book(Long id, String title, String publisher, Integer numberOfPages, Integer yearOfPublication,
+			String synopsis, String isbn10, String isbn13) {
+		this.id = id;
+		this.title = title;
+		this.publisher = publisher;
+		this.numberOfPages = numberOfPages;
+		this.yearOfPublication = yearOfPublication;
+		this.synopsis = synopsis;
+		this.isbn10 = isbn10;
+		this.isbn13 = isbn13;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getPublisher() {
+		return publisher;
+	}
+
+	public void setPublisher(String publisher) {
+		this.publisher = publisher;
+	}
+
+	public Integer getNumberOfPages() {
+		return numberOfPages;
+	}
+
+	public void setNumberOfPages(Integer numberOfPages) {
+		this.numberOfPages = numberOfPages;
+	}
+
+	public Integer getYearOfPublication() {
+		return yearOfPublication;
+	}
+
+	public void setYearOfPublication(Integer yearOfPublication) {
+		this.yearOfPublication = yearOfPublication;
+	}
+
+	public String getSynopsis() {
+		return synopsis;
+	}
+
+	public void setSynopsis(String synopsis) {
+		this.synopsis = synopsis;
+	}
+
+	public String getIsbn10() {
+		return isbn10;
+	}
+
+	public void setIsbn10(String isbn10) {
+		this.isbn10 = isbn10;
+	}
+
+	public String getIsbn13() {
+		return isbn13;
+	}
+
+	public void setIsbn13(String isbn13) {
+		this.isbn13 = isbn13;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Book other = (Book) obj;
+		return Objects.equals(id, other.id);
+	}
+	
+	
+	
+	
+}
